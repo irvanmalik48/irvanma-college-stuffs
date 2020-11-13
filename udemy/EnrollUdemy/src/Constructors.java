09c446e6cd73da06b92b3060cd97b2e499cd17a5
Irvan Malik <71539547+irvanmalik48@users.noreply.github.com>
2020-11-14 00:28:59 +0700

    .

---
+++ udemy/EnrollUdemy/src/Constructors.java
@@ -0,0 +1,38 @@
+class Machine
+{
+	private String name;
+	private int code;
+	
+	public Machine()
+	{
+		this("Eum", 99);
+		System.out.println("Constructor running.");
+	
+		name = "Tanta";
+	}
+	
+	public Machine(String name)
+	{
+		this(name, 0);
+		System.out.println("Second constructor running.");
+	
+		this.name = name;
+	}
+	
+	public Machine(String name, int code)
+	{
+		System.out.println("Third constructor running.");
+		this.name = name;
+		this.code = code;
+	}
+}
+
+public class Constructors 
+{
+	public static void main(String[] args) 
+	{
+		Machine machine1 = new Machine();
+		Machine machine2 = new Machine("Syifa");
+		Machine machine3 = new Machine("Syifa", 99);
+	}
+}
---
+++ udemy/EnrollUdemy/src/ToStringMethod.java
@@ -0,0 +1,16 @@
+class FrogH
+{
+	public String toString()
+	{
+		return "Hello";
+	}
+}
+
+public class ToStringMethod 
+{
+	public static void main(String[] args) 
+	{
+		FrogH frog1 = new FrogH();
+		System.out.println(frog1);
+	}
+}
